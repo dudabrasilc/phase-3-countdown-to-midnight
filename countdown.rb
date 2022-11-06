e374693b588945a848022c4eaf906cb4e55c3190
Maria Coitinho <brasildu@gmail.com>
2022-11-06 03:11:59 -0600

    commit tree 88126a25bfeb6fb775040ad75a1eae73ce5b8dee

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+require 'pry'
+
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  sleep number
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+binding.pry
+
